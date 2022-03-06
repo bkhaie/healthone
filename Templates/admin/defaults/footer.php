e8e6ea558dd7a1f4a1ea09cc2cd6405fa365b799
Badr <70712849+bkhaie@users.noreply.github.com>
2022-03-07 00:07:16 +0100

    push!

---
+++ Templates/admin/defaults/footer.php
@@ -0,0 +1,3 @@
+<footer >
+    <p class="clearfix pb-3 text-primary text-center">2021 Health one&reg;</p>
+</footer>
---
+++ Templates/home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+    <html>
+    <?php
+    include_once('defaults/head.php');
+    ?>
+    <body>
+        <div class="container">
+            <?php
+            include_once ('defaults/header.php');
+            include_once ('defaults/menu.php');
+            include_once ('defaults/pictures.php');
+            ?>
+
+            <h1 class="text-center">| Sportcenter Health<span class="text-primary">One</span> |</h1>
+            <br>
+
+    
+            <br>
+            <div class="row">
+                <div class="col-md-12">
+                    - Fit en gezond zijn is geen vanzelfsprekendheid. We moeten er zelf wat voor doen. Goede, gezonde voeding is hiervoor de basis.
+                Bewegen hoort hier ook bij. Regelmatig bewegen zorgt voor een goede doorbloeding en draagt bij aan ontspanning van lichaam en geest.
+                Sporten is goed voor sterkere spieren en voor de conditie. Sporcenter HealthOne heeft verschillende sportapparaten om mee te kunnen werken aan je conditie.
+               
+            </div>
+
+            <br><hr><br>
+
+            <?php
+            include_once ('defaults/footer.php');
+            ?>
+        </div>
+    </body>
+    <script src="../js/home.js"></script>
+</html>
